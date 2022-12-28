f74eb98070cc0e68d08769132a933e8d42e768b5
이준혁 <ballack02@naver.com>
2022-12-28 13:23:41 +0900

    axios 로 backend api 통신 및 회원가입 테스트

--- vue-til/src/components/SignupForm.vue
+++ vue-til/src/components/SignupForm.vue
@@ -1,34 +1,59 @@
 <template>
   <form @submit.prevent="submitForm">
     <div>
-      <label for="username">id: </label>
+      <label for="username">아이디: </label>
       <input id="username" type="text" v-model="username" autocomplete="off" />
     </div>
     <div>
-      <label for="password">pw: </label>
+      <label for="password">패스워드: </label>
       <input id="password" type="text" v-model="password" autocomplete="off" />
     </div>
     <div>
-      <label for="nickname">nickname: </label>
+      <label for="nickname">별명: </label>
       <input id="nickname" type="text" v-model="nickname" autocomplete="off" />
     </div>
-    <button type="submit">SignUp</button>
+    <button type="submit">가입하기</button>
+    <p>
+      {{ logMessage }}
+    </p>
   </form>
 </template>
 
 <script>
+  import { registerUser } from "@/api/index";
+
   export default {
     data() {
       return {
+        // form values
         username: "",
         password: "",
         nickname: "",
+        // log
+        logMessage: "",
       };
     },
     methods: {
-      submitForm(event) {
+      async submitForm() {
         console.log("form sended");
-        console.log(event);
+        const userData = {
+          username: this.username,
+          password: this.password,
+          nickname: this.nickname,
+        };
+        try {
+          const { data } = await registerUser(userData);
+          console.log(data.username);
+          this.logMessage = `${data.username}님이 가입되었습니다.`;
+          this.initForm();
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      initForm() {
+        this.username = "";
+        this.password = "";
+        this.nickname = "";
       },
     },
   };
