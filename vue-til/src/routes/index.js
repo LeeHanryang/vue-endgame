ba57fda10d97d3159864f2a4e9568066c7fc91a3
이준혁 <ballack02@naver.com>
2022-12-28 10:52:26 +0900

    예외 페이지 설정(404)

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -20,5 +20,9 @@ export default new VueRouter({
       path: "/signup",
       component: () => import("@/views/SignupPage"),
     },
+    {
+      path: "*",
+      component: () => import("@/views/NotFoundPage"),
+    },
   ],
 });
