790dc1d1b9c0ec87c585510fc55c1630bb9d9623
이준혁 <ballack02@naver.com>
2023-01-05 10:02:51 +0900

    list styling / Loading Spinner

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -1,27 +1,39 @@
 <template>
-  <div>
-    <h1>Today I Learned</h1>
-    <ul>
-      <li v-for="postItem in postItems" :key="postItem._id">
-        {{ postItem }}
-      </li>
+  <div class="main list-container contents">
+    <h1 class="page-header">Today I Learned</h1>
+    <loading-spinner v-if="isLoading"></loading-spinner>
+    <ul v-else>
+      <post-list-item
+        v-for="postItem in postItems"
+        :key="postItem._id"
+        :postItem="postItem"
+      >
+      </post-list-item>
     </ul>
   </div>
 </template>
 
 <script>
+  import PostListItem from "@/components/posts/PostListItem";
   import { fetchPosts } from "@/api/index";
+  import LoadingSpinner from "@/components/common/LoadingSpinner";
 
   export default {
     data() {
       return {
         postItems: [],
+        isLoading: false,
       };
+    },
+    components: {
+      PostListItem,
+      LoadingSpinner,
     },
     methods: {
       async fetchData() {
+        this.isLoading = true;
         const { data } = await fetchPosts();
-        console.log(data);
+        this.isLoading = false;
         this.postItems = data.posts;
       },
     },
