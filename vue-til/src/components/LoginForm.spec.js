29aedf636437b15069f76278d7763f26a47f9c10
이준혁 <ballack02@naver.com>
2023-01-09 09:08:17 +0900

    jest/ describe, expect, toBe, not

---
+++ vue-til/src/components/LoginForm.spec.js
@@ -0,0 +1,10 @@
+import { sum } from "./math";
+
+// sum(10, 20); // 30
+
+describe("math.js", () => {
+  test("10 + 20 = 30", () => {
+    const result = sum(10, 20);
+    expect(result).not.toBe(15);
+  });
+});
